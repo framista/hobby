6015374defac3b76c90ccb75f5e78b3ae2f1a40c
framista <adajab1@gmail.com>
2020-02-10 23:45:15 +0100

    Images using unsplash api

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
+import { APP_ACCESS_KEY } from './password'
+
 const tabs = document.querySelectorAll('.tab')
+const images = document.querySelectorAll('.image')
+const queries = ['plane', 'cat', 'nature', 'programming']
 
-console.log(tabs)
-tabs.forEach(tab => {
+tabs.forEach((tab, index) => {
     tab.addEventListener('click', () => {
-
         tabs.forEach(tab => {
             tab.classList.remove('tab--active')
         })
         tab.classList.add('tab--active')
+        unsplashImage(queries[index])
     })
 })
+
+function unsplashImage(query) {
+    fetch(`https://api.unsplash.com/search/photos?client_id=${APP_ACCESS_KEY}&query=${query}`, { method: 'get' }).
+        then(res => res.json())
+        .then(res => {
+            images.forEach( (image, index) => {
+                image.style.backgroundImage = `url(${res.results[index].urls.regular}`;
+            })  
+        })
+        .catch(res => console.log("error", res))
+}
+
+unsplashImage("planes");
